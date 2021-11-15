7a2d5cc89e747a300d094f00a44f109e985ac32d
陳柏穎 <bycxen@gmail.com>
2021-11-15 10:02:23 +0800

    show load limit

--- dp.py
+++ dp.py
@@ -1,4 +1,13 @@
-weather = ['HT', 'HT', 'Sunny', 'Storm', 'Sunny', 'HT', 'Storm', 'Sunny', 'HT', 'HT', 'Storm', 'HT', 'Sunny', 'HT', 'HT', 'HT', 'Storm', 'Storm', 'HT', 'HT', 'Sunny', 'Sunny', 'HT', 'Sunny', 'Storm', 'HT', 'Sunny', 'Sunny', 'HT', 'HT']
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+weather = [
+    'HT', 'HT', 'Sunny', 'Storm', 'Sunny', 'HT', 
+    'Storm', 'Sunny', 'HT', 'HT', 'Storm', 'HT', 
+    'Sunny', 'HT', 'HT', 'HT', 'Storm', 'Storm', 
+    'HT', 'HT', 'Sunny', 'Sunny', 'HT', 'Sunny', 
+    'Storm', 'HT', 'Sunny', 'Sunny', 'HT', 'HT'
+]
 E = {
     1: [2, 25],
     2: [3],
@@ -32,9 +41,11 @@ V = 15
 M = 12
 N = len(E)
 T = 30
-load_limit = 200
 # load_limit = 1200
+load_limit = 200
+# initial_money = 10000
 initial_money = 2000
+# bonus_income = 1000
 bonus_income = 10000
 quality = [3, 2]
 price = [5, 10]
@@ -49,18 +60,23 @@ consumption = {
 DP = {}
 
 water_range = range(load_limit // quality[0] + 1)
-food_range = lambda w: range((load_limit - w * quality[0]) // quality[1])
+food_range = lambda w: range((load_limit - w * quality[0]) // quality[1] + 1)
 
 for d in range(T+1):
     for p in range(1, N+1):
         for w in water_range:
             for f in food_range(w):
-                DP[(d,p,w,f)] = (float('-inf'), (), '', '')
+                DP[(d,p,w,f)] = (float('-inf'), (), '', '', 0)
+                
 
+
+# %%
 for w in water_range:
     for f in food_range(w):
-        DP[(0,1,w,f)] = (initial_money - price[0] * w - price[1] * f, (), '', '')
+        DP[(0,1,w,f)] = (initial_money - w * price[0] - f * price[1], (), '', '', w * quality[0] + f * quality[1])
 
+
+# %%
 for d in range(T):
     for p in range(1, N+1):
         if p == V:
@@ -68,16 +84,17 @@ for d in range(T):
                 for f in food_range(w):
                     capacity = load_limit - w * quality[0] - f * quality[1]
                     wb_range = range(capacity // quality[0] + 1)
-                    fb_range = lambda w: range((capacity - w * quality[0]) // quality[1])
+                    fb_range = lambda w: range((capacity - w * quality[0]) // quality[1] + 1)
                     for wb in wb_range:
                         for fb in fb_range(wb):
                             key = (d, V, w + wb, f + fb)
-                            if  DP[(d,p,w,f)][0] - 2 * wb * price[0] - 2 * fb * price[1] >= 0 and DP[key][0] < DP[(d,p,w,f)][0] - 2 * wb * price[0] - 2 * fb * price[1]:
+                            if DP[(d,p,w,f)][0] - 2 * wb * price[0] - 2 * fb * price[1] >= 0                                 and DP[key][0] < DP[(d,p,w,f)][0] - 2 * wb * price[0] - 2 * fb * price[1]:
                                 DP[key] = (
                                     DP[(d,p,w,f)][0] - 2 * wb * price[0] - 2 * fb * price[1], 
                                     (d,p,w,f), 
                                     'purchase', 
-                                    'Village'
+                                    'Village',
+                                    w * quality[0] + f * quality[1]
                                 )
         for w in water_range:
             for f in food_range(w):
@@ -88,7 +105,8 @@ for d in range(T):
                             DP[(d,p,w,f)][0], 
                             (d,p,w,f), 
                             'stay', 
-                            weather[d]
+                            weather[d],
+                            w * quality[0] + f * quality[1]
                         )
         for w in water_range:
             for f in food_range(w):
@@ -100,7 +118,8 @@ for d in range(T):
                                 DP[(d,p,w,f)][0], 
                                 (d,p,w,f), 
                                 'go', 
-                                weather[d]
+                                weather[d],
+                                w * quality[0] + f * quality[1]
                             )
         if p == M:
             for w in water_range:
@@ -112,9 +131,12 @@ for d in range(T):
                                 DP[(d,p,w,f)][0] + bonus_income, 
                                 (d,p,w,f), 
                                 'mine', 
-                                weather[d]
+                                weather[d],
+                                w * quality[0] + f * quality[1]
                             )
 
+
+# %%
 DP_end = {key: DP[key] for key in DP if key[1] == N}
 maxIdx = max(DP_end, key=DP_end.get)
 print(f'{maxIdx}: {DP_end[maxIdx]}')
@@ -122,3 +144,5 @@ prevKey = DP_end[maxIdx][1]
 while len(prevKey) != 0:
     print(f'{prevKey}: {DP[prevKey]}')
     prevKey = DP[prevKey][1]
+
+
